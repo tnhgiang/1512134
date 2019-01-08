090e64a7cf4e64ec32f9c11edf3a44b6d0727342
tnhgiang <45427365+tnhgiang@users.noreply.github.com>
2019-01-08 22:15:04 +0700

    Add files via upload

--- sobel.js
+++ sobel.js
@@ -9,13 +9,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
   vid.addEventListener('play', function(){
       cvs_w = vid.clientWidth;
-      cvs_h = vid.clientHeight;
-    //   cvs.width = cvs_w;
-    //   cvs.height = cvs_h;
-    //   back.width = cvs_w;
-    //   back.height = cvs_h;
-      
-      
+      cvs_h = vid.clientHeight;     
       draw(vid, ctx, cvs_w, cvs_h);
   },false);
 }, false);
@@ -41,7 +35,7 @@ function draw(v, c, cw, ch) {
   var kernel_x = [+1, 0, -1, +2, 0, -2, +1, 0, -1];
   var kernel_y = [+1, +2, +1, 0, 0, 0, -1, -2, -1];
   var data = [];
-  convolution(data,idata.data,cw,kernel_x, kernel_y);  //Đạo hàm theo truc x
+  convolution(data,idata.data,cw,kernel_x, kernel_y);
 
   for (var i = 0; i < idata.data.length; i++) {
             idata.data[i] = data[i];
@@ -53,13 +47,6 @@ function draw(v, c, cw, ch) {
   setTimeout(draw,0,v,c,cw,ch);
 }
 
-// Tính tích chập theo kernel đưa vào
-// - data lưu dữ liệu đã biến đổi
-// - idata là dữ liệu nguồn
-// - w là chiều rộng ảnh đề tính widthstep
-// - widthstep dùng để điều chỉnh lên dòng hay xuống dòng
-// - kernel_step dùng để điều chỉnh lùi cột hay tiến cột 
-// - i_local là biến chỉ vị trí xung quanh i 
 function convolution(data, idata, w, kernel_x,kernel_y, opaque=true, threshold=0){
 
   var kernel_size = Math.sqrt(kernel_x.length); 
@@ -74,9 +61,6 @@ function convolution(data, idata, w, kernel_x,kernel_y, opaque=true, threshold=0
       for(var k = 0; k < kernel_x.length; k+=1) {    
         var widthstep = w*kernel_step[k]*4;
         var i_local = i + widthstep + kernel_step[k]*4;
-
-        // if(kernel_step[k]==0 && widthstep==0) continue;
-
         if(( i_local >= 0) && (i_local < idata.length)){
             var x = idata[i_local];
             var y_x = kernel_x[k];
